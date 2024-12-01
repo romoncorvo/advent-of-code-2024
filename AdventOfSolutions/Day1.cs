c61040e41d11c3cc66d40a1f9f4d418c517d1ee7
romoncorvo <36642718+romoncorvo@users.noreply.github.com>
2024-12-01 17:05:06 +0100

    Day 1 part 1

---
+++ AdventOfSolutions/Day1.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfSolutions;
+
+public class Day1
+{
+    public static int FindListDistance(string fileName)
+    {
+        var unparsedFile = File.ReadAllLines(fileName);
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        
+        foreach (var line in unparsedFile)
+        {
+            var split = line.Split("  ");
+            list1.Add(int.Parse(split[0]));
+            list2.Add(int.Parse(split[1]));
+        }
+        
+        list1.Sort();
+        list2.Sort();
+        var distance = 0;
+
+        for (var i = 0; i < list1.Count; i++)
+        {
+            distance += Math.Abs(list1[i] - list2[i]);
+        }
+        
+        return distance;
+    }
+}
---
+++ AdventOfTests/Day1/Day1Tests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit.Abstractions;
+using static AdventOfSolutions.Day1;
+
+namespace AdventOfTests.Day1;
+
+public class Day1Tests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public Day1Tests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void FindListDistance_WithExampleInput_Returns11()
+    {
+        // Arrange
+        var filePath = "./Day1/day-1-1-example.txt";
+        
+        // Act
+        var result = FindListDistance(filePath);
+        
+        // Assert
+        Assert.Equal(11, result);
+    }
+    
+    [Fact]
+    public void FindListDistance_WithRealInput_ReturnsAnswer()
+    {
+        // Arrange
+        var filePath = "./Day1/day-1-1-input.txt";
+        
+        // Act
+        var result = FindListDistance(filePath);
+        
+        // Assert
+        _testOutputHelper.WriteLine(result.ToString());
+    }
+}
